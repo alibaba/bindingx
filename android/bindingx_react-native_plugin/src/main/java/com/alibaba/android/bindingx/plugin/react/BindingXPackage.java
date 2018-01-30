4e8c122720a1358fefcaa43768d9c6a21b7a1b4c
chuyi <jianjian.sjj@alibaba-inc.com>
2018-01-30 09:53:30 +0800

    chore(android): update build config

---
+++ android/bindingx_react-native_plugin/src/main/java/com/alibaba/android/bindingx/plugin/react/BindingXPackage.java
@@ -0,0 +1,33 @@
+//package com.alibaba.android.bindingx.plugin.react;
+//
+//import com.facebook.react.ReactPackage;
+//import com.facebook.react.bridge.JavaScriptModule;
+//import com.facebook.react.bridge.NativeModule;
+//import com.facebook.react.bridge.ReactApplicationContext;
+//import com.facebook.react.uimanager.ViewManager;
+//
+//import java.util.List;
+//
+///**
+// * Description:
+// *
+// * Created by rowandjj(chuyi)<br/>
+// */
+//
+//public class BindingXPackage implements ReactPackage{
+//
+//    @Override
+//    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Class<? extends JavaScriptModule>> createJSModules() {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+//        return null;
+//    }
+//}
