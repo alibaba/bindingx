3c9a72c65695de064e81bf98742d9bfb3b2f01c2
对象 <duixiang.hjh@alibaba-inc.com>
2018-02-09 12:36:27 +0800

    remove alibaba info

--- ios/playground/BindingDemo/AppDelegate.m
+++ ios/playground/BindingDemo/AppDelegate.m
@@ -33,16 +33,7 @@
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
-//    NSString* path = [NSString stringWithFormat:@"file://%@/bundlejs/index.js?wh_weex=true",[NSBundle mainBundle].bundlePath];
-//    NSString* path = @"http://dotwe.org/raw/dist/a02b3b3d6e0983be733b3bb2f1e4140f.bundle.wx?_wx_tpl=http://dotwe.org/raw/dist/a02b3b3d6e0983be733b3bb2f1e4140f.bundle.wx";//timing
-    
-//    NSString* path = @"http://dotwe.org/raw/dist/19229fdaff971b2e29682d4c3d13f29d.bundle.wx?_wx_tpl=http://dotwe.org/raw/dist/19229fdaff971b2e29682d4c3d13f29d.bundle.wx";//pan
-    
-//    NSString* path = @"http://rax.alibaba-inc.com/bundle/ce009739-b14d-4a06-b239-7977452c5232/bundle.js?wh_weex=true&wh_ttid=native&_wx_tpl=http://rax.alibaba-inc.com/bundle/ce009739-b14d-4a06-b239-7977452c5232/bundle.js";//scroller
-    
-//    NSString* path = @"http://rax.alibaba-inc.com/bundle/178b55d8-f64f-434f-89b0-007ea5740d8a/bundle.js?wh_weex=true&wh_ttid=native&_wx_tpl=http://rax.alibaba-inc.com/bundle/178b55d8-f64f-434f-89b0-007ea5740d8a/bundle.js";//gyro
-    
-    NSString* path = @"http://dotwe.org/raw/dist/2b2e2cf98d6de00e9f022de097955f6f.bundle.wx?_wx_tpl=http://dotwe.org/raw/dist/2b2e2cf98d6de00e9f022de097955f6f.bundle.wx";//color
+    NSString* path = [NSString stringWithFormat:@"file://%@/bundlejs/index.js?wh_weex=true",[NSBundle mainBundle].bundlePath];
     
     WXViewController *controller = [[WXViewController alloc] initWithURL:[NSURL URLWithString:path]];
     WXRootViewController *rootController = [[WXRootViewController alloc] initWithRootViewController:controller];
