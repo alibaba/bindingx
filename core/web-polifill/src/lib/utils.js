888e284223f3f9c865a5f400bdceccd957ff6517
xiaoqi.huxq <xiaoqi.huxq@alibaba-inc.com>
2018-02-13 16:24:10 +0800

    fix: add liscense to js files

--- core/web-polifill/src/lib/utils.js
+++ core/web-polifill/src/lib/utils.js
@@ -1,11 +1,27 @@
 /**
+ Copyright 2018 Alibaba Group
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+/**
  * Transforms matrix into an object
  *
  * @param string matrix
  * @return object
  */
 
-// TODO matrix4 3D场景下待实现  目前仅仅实现matrix  2D
+// TODO matrix4 for 3D
 const matrixToTransformObj = function(matrix) {
   // this happens when there was no rotation yet in CSS
   if (matrix === 'none') {
