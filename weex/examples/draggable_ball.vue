ab3ca2ed629b43337aaeb89cdd74b75f79147cf2
chuyi <jianjian.sjj@alibaba-inc.com>
2018-03-16 16:01:40 +0800

    feat: add vue demo

---
+++ weex/examples/draggable_ball.vue
@@ -0,0 +1,158 @@
+<template>
+	<div class="container">
+		<div ref="ball" class="ball" @touchstart="onPanStart" />
+    <div class="desc_wrapper">
+      <text class="desc">Draggable Ball</text>
+    </div>
+	</div>
+</template>
+
+<style>
+	.container{
+		flex: 1;
+    background-color: #00B0FF;
+	}
+
+  .ball {
+    width:100;
+    height: 100;
+    border-radius: 50;
+    background-color: #00ff00;
+    margin:48;
+  }
+
+  .desc {
+    color: #ffffff;
+    font-size: 35;
+  }
+
+  .desc_wrapper {
+    position: absolute;
+    bottom: 0;
+    width: 750;
+    height: 100;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
+
+<script>
+  import Binding from 'weex-bindingx';
+
+	module.exports = {
+		data: {
+      x : 0,
+      y : 0,
+      isInAnimation : false,
+			gesToken: 0
+		},
+		methods: {
+
+			getEl: function(e) {
+					return e.ref;
+			},
+			onPanStart: function(e) {
+        if (this.isInAnimation === true) {
+          console.log("we are in animation, drop pan gesture...")
+          if (this.gesToken != 0) {
+            Binding.unbind({
+              eventType: 'pan',
+              token: this.gesToken
+            })
+            this.gesToken = 0;
+          }
+          return
+        }
+
+        var my = this.getEl(this.$refs.ball);
+        var expression_x_origin = `x+${this.x}`;
+        var expression_y_origin = `y+${this.y}`;
+
+        var gesTokenObj = Binding.bind({
+          anchor: my,
+          eventType: 'pan',
+          props: [
+            {
+              element: my,
+              property: 'transform.translateX',
+              expression: expression_x_origin
+            },
+            {
+              element: my,
+              property: 'transform.translateY',
+              expression: expression_y_origin
+            }
+          ]
+        }, (e) => {
+          if (e.state === 'end') {
+            this.x += e.deltaX;
+            this.y += e.deltaY;
+
+            //anim
+            this.bindTiming();
+          }
+        })
+
+        this.gesToken = gesTokenObj.token;
+			},
+			bindTiming: function() {
+				this.isInAnimation = true;
+				var my = this.getEl(this.$refs.ball);
+
+				//should equal with timing duration
+				var exit_origin = "t>1000";
+
+				var changed_x;
+				var final_x;
+				var final_y;
+				if (this.x > (750 - 50 * 2) / 2) {//right
+					changed_x = 550 - this.x;
+					final_x = 550;
+				} else {
+					changed_x = 0 - this.x;
+					final_x = 0;
+				}
+
+				var totalHeight = 1000;
+				var changed_y;
+				if (this.y > totalHeight / 2) {
+					changed_y = totalHeight - 200 - this.y;
+					final_y = totalHeight - 200;
+				} else {
+					changed_y = 0 - this.y;
+					final_y = 0;
+				}
+
+				var expression_x = `easeOutElastic(t,${this.x},${changed_x},1000)`;
+				var expression_y = `easeOutElastic(t,${this.y},${changed_y},1000)`;
+
+				var result = Binding.bind({
+					eventType: 'timing',
+					exitExpression: exit_origin,
+					props: [
+						{
+							element: my,
+							property: 'transform.translateX',
+							expression: expression_x
+						},
+						{
+							element: my,
+							property: 'transform.translateY',
+							expression: expression_y
+						}
+					]
+
+				}, (e) => {
+					if (e.state === 'end' ||
+						e.state === 'exit') {
+						// reset x & y
+						this.x = final_x;
+						this.y = final_y;
+						this.isInAnimation = false;
+					}
+				})
+
+			}
+		}
+	}
+</script>
--- weex/examples/index.vue
+++ weex/examples/index.vue
@@ -1,7 +1,12 @@
 <template>
 	<div class="conatiner">
-		<text style="margin-bottom: 20px;">weex plugin examples</text>
-		<div @click="createAction" style="margin: 20px;padding:20px;background-color:#1ba1e2;color:#fff;"><text style="color:#fff">hello world</text></div>
+		<list class="list" >
+			<cell v-for="item in items">
+					<div @click="clickItem(item.url)" class="card">
+							<text class="title">{{item.title}}</text>
+					</div>
+			</cell>
+		</list>
 	</div>
 </template>
 
@@ -9,21 +14,76 @@
 	.container{
 		flex: 1;
 	}
+
+	.list {
+	  flex:1;
+		position:absoulte;
+		top:0;
+		background-color:#f2f3f4;
+	}
+
+  .card {
+		width: 710;
+	  height: 150;
+	  background-color:#ffffff;
+	  border-radius: 25;
+	  margin: 20;
+	  padding: 20;
+	  justify-content:center;
+	  align-items:center;
+
+	}
+
+	.title {
+		font-size: 35;
+	}
 </style>
 
 <script>
+	const modal = weex.requireModule('modal')
+	const navigator = weex.requireModule('navigator')
 
-	const plugin = weex.requireModule('bindingx');
+	const root = weex.config.bundleUrl;
+
 	module.exports = {
 		data: {
-			value: '',
-			index: 0,
-			txtChange: ''
+			items: [
+					{
+						title:'Draggable Ball',
+						url:'draggable_ball.js'
+					},
+					{
+						title:'Swipeable Card',
+						url:'swipeable_card.js'
+					},
+					{
+						title:'Expandable Menu',
+						url:'https://jsplayground.taobao.com/bundle/073b10f5-a4fd-41dc-9265-6865458d1043/raxbundle.js?wh_weex=true&wh_ttid=native&_wx_tpl=https://jsplayground.taobao.com/bundle/073b10f5-a4fd-41dc-9265-6865458d1043/raxbundle.js'
+					},
+					{
+						title:'Slide Layout',
+						url:'https://jsplayground.taobao.com/bundle/a62b34cb-9174-4667-83d8-3f452d6a1829/raxbundle.js?wh_weex=true&wh_ttid=native&_wx_tpl=https://jsplayground.taobao.com/bundle/a62b34cb-9174-4667-83d8-3f452d6a1829/raxbundle.js'
+					},
+					{
+						title:'Circle Menu',
+						url:'https://jsplayground.taobao.com/bundle/e04fd2ca-f3f6-4a4c-8500-0d784168bd77/raxbundle.js?wh_weex=true&wh_ttid=native&_wx_tpl=https://jsplayground.taobao.com/bundle/e04fd2ca-f3f6-4a4c-8500-0d784168bd77/raxbundle.js'
+					},
+					{
+						title:'Navigation with Tab',
+						url:'https://jsplayground.taobao.com/bundle/af2aba4c-d805-4545-a2d1-9f9db0bf2b50/raxbundle.js?wh_weex=true&wh_ttid=native&_wx_tpl=https://jsplayground.taobao.com/bundle/af2aba4c-d805-4545-a2d1-9f9db0bf2b50/raxbundle.js&wx_navbar_hidden=true'
+					},
+					{
+						title:'Ripple Effect',
+						url:'https://jsplayground.taobao.com/bundle/c295cf39-ec9e-4128-a968-fb051fb48022/raxbundle.js?wh_weex=true&wh_ttid=native&_wx_tpl=https://jsplayground.taobao.com/bundle/c295cf39-ec9e-4128-a968-fb051fb48022/raxbundle.js'
+					}
+			  ]
 		},
 		methods: {
-			createAction: function() {
-				plugin.show();
-
+			clickItem: function(e) {
+				 navigator.push({
+				 		url:root.replace('index.js',e),
+						animated:'true'
+				 }, function(){})
 			}
 		}
 	}
