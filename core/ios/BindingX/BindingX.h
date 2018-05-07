b267456b6aa553b37e063deb0852b300046d3f65
对象 <duixiang.hjh@alibaba-inc.com>
2018-05-07 15:23:51 +0800

    chore: public BindingX header

--- core/ios/BindingX/BindingX.h
+++ core/ios/BindingX/BindingX.h
@@ -6,12 +6,8 @@
 //  Copyright © 2018年 weexplugin. All rights reserved.
 //
 
-//! Project version number for BindingX.
 FOUNDATION_EXPORT double BindingXVersionNumber;
 
-//! Project version string for BindingX.
 FOUNDATION_EXPORT const unsigned char BindingXVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <WeexEnhance/PublicHeader.h>
 
 #import <BindingX/EBHandlerFactory.h>
