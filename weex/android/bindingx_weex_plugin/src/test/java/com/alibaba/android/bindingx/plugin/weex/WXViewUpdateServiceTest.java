456a7e72532424980d178e0a80e9418db7582407
chuyi <jianjian.sjj@alibaba-inc.com>
2018-05-08 12:55:54 +0800

    chore(android): fix ut

--- weex/android/bindingx_weex_plugin/src/test/java/com/alibaba/android/bindingx/plugin/weex/WXViewUpdateServiceTest.java
+++ weex/android/bindingx_weex_plugin/src/test/java/com/alibaba/android/bindingx/plugin/weex/WXViewUpdateServiceTest.java
@@ -16,9 +16,14 @@
 package com.alibaba.android.bindingx.plugin.weex;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
 public class WXViewUpdateServiceTest {
 
     @Test
