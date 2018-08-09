bb6307f47fc1469be6140a13d1f23a41e0f0b3dc
chuyi <jianjian.sjj@alibaba-inc.com>
2018-08-09 11:22:23 +0800

    chore: init bindingx-native plugin

---
+++ native/android/bindingx_android_plugin/changelog.md
@@ -0,0 +1,5 @@
+### bindingx-android-plugin changelog
+
+#### 1.0.0
+
+1. 初始版本。
---
+++ native/android/bindingx_android_plugin/src/main/java/com/alibaba/android/bindingx/plugin/android/NativeBindingX.java
@@ -0,0 +1,9 @@
+package com.alibaba.android.bindingx.plugin.android;
+
+/**
+ * Description:
+ *
+ * Created by rowandjj(chuyi)<br/>
+ */
+public class NativeBindingX {
+}
